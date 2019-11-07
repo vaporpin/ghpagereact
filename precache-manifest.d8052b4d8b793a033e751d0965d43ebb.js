7a8cb193eee59881a2bc98fb70e906a2da48605e
Fei Gao <feigao@Bellatrix.local>
2019-11-07 13:10:34 +0800

    Updates

---
+++ precache-manifest.d8052b4d8b793a033e751d0965d43ebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d67737ad4a91c732ff8839946ba99d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e29defeabac9a22fe03",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "389f12867b48e5515ed3",
+    "url": "/static/js/2.f80d4e21.chunk.js"
+  },
+  {
+    "revision": "8e29defeabac9a22fe03",
+    "url": "/static/js/main.e7339768.chunk.js"
+  },
+  {
+    "revision": "8a1881e95b23123e001c",
+    "url": "/static/js/runtime-main.1faa10d8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
