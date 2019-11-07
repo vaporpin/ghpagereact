14070999022d012bb7d247a3e4cc9e83653b1e5c
Fei Gao <feigao@Bellatrix.local>
2019-11-07 13:35:15 +0800

    new logo

--- src/App.js
+++ src/App.js
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          An <b>autonomous AI</b> platform to create, deploy and update AI models for <b>domain-specific applications</b>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
